418914a707b55fc77c059cb18bafdcb818dfabc0
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-13 20:06:40 +0600

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[14]:
-
-
 import pandas as pd 
 import numpy as np
 from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix
 from sklearn import svm
-
 import itertools
-
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
-
-
 import seaborn
 
 get_ipython().run_line_magic('matplotlib', 'inline')
