a5b781e6435f3b43cd52b460b2ae515656b957c6
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-13 20:08:14 +0600

    Update Decision Tree.py

--- Decision Tree.py
+++ Decision Tree.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -21,22 +15,12 @@ sns.set()
 
 mydata = pd.read_csv('D:\\445\\Khidmahactuall.csv')
 
-
-# In[2]:
-
-
 X = mydata.drop('Diagnosis', axis=1)
 y = mydata['Diagnosis']
 
 
-# In[3]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=50, stratify=y)
-
-
-# In[6]:
 
 
 depth = np.arange(1, 9)
@@ -53,40 +37,19 @@ plt.xlabel('Depth of tree')
 plt.ylabel('Accuracy')
 plt.show()
 
-
-# In[7]:
-
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-
-# In[8]:
-
 
 classifier = DecisionTreeClassifier()
 
 
-# In[9]:
-
-
 classifier.fit(X_train, y_train)
-
-
-# In[10]:
-
 
 predict = classifier.predict(X_test)
 
 
-# In[11]:
-
-
 from sklearn import metrics
 print("Accuracy:",metrics.accuracy_score(y_test, predict))
-
-
-# In[ ]:
 
 
 
