a07b94d5ed014d3b535f671f9cca628afe23eb75
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-09 22:12:40 +0600

    Update Check mean of a column and check missing values.py

--- Check mean of a column and check missing values.py
+++ Check mean of a column and check missing values.py
@@ -1,10 +1,13 @@
 import pandas as pd
 d = pd.read_csv("D:\\445\\13.csv")
 meanvalue = d['Age'].mean()
+
 print ('Mean Age: ' + str(meanvalue))
 import pandas as pd
+
 import numpy as np
 df = pd.read_csv("D:\\445\\13.csv")
+
 
 print (df.isnull().sum())
 
