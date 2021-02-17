6701890a7fc0b4e94e65ec3c9ca33d9ecd2db029
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-17 20:25:00 +0600

    Update Decision Tree.py

--- Decision Tree.py
+++ Decision Tree.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import seaborn as sns
 import re
 import numpy as np
-
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
