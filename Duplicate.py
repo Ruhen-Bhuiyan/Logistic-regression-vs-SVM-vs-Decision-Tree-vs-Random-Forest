43a019d159974a947203abbaaa74a9a669e53567
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-05-07 23:46:54 +0600

    Update Duplicate.py

--- Duplicate.py
+++ Duplicate.py
@@ -1,9 +1,7 @@
 import pandas as pd
 mydata = pd.read_csv("D:\\445\\13.csv")
 mydata.duplicated(subset=None, keep='first')
-
 duplicatevalues = mydata[mydata.duplicated(['Age'])]
-
 print("Duplicate Rows based on a single column are:", duplicatevalues, sep='\n')
 duplicatevalues = mydata[mydata.duplicated(['Age', 'Smoke'])]
 print("Duplicate Rows based on 2 columns are:", duplicatevalues, sep='\n')
